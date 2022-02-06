54130c73f0e3e291d0c2aa4450327c0129be9146
Luca StockWil <luca.stockwil@gmail.com>
2022-02-06 17:49:29 -0600

    Use partial instead of lambda for commands.

--- player.py
+++ player.py
@@ -3,6 +3,8 @@ class Player:
         self.name = "NONE" #string for player's name
         self.my_board = [] # board to show player's full board
         self.enemy_board = [] #board to show player this player has marked on the other player's board
+                              # all buttons in my_board array will be DISABLED so you can't click your own grid
+
          #A board will be a 100-element array that will stores the Tkinter IDs of each button
         #Ex:
         # [a1][a2][a3]....[b10]
@@ -12,4 +14,5 @@ class Player:
         # .
         # .
         # [j1][j2][j3]....[j10]
+
     
--- start.py
+++ start.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from functools import partial
+from itertools import product
+
 from player import Player
 
 def show_frame(frame):
@@ -11,7 +13,8 @@ num_ships = 0
 player1 = Player() #initialize players
 player2 = Player()
 ###
-
+positions = product(range(10), range(10))
+button_ids = []
 
 
 root = Tk()
@@ -79,32 +82,42 @@ def int_to_char(x): #converts given integer into to a character
 def char_to_int(x): #converts given character into an integer
     return int(x) - 64
 
-def changeBoard(): #helper function for board 
-    return
 
-def board(type):
+# @drawBoard:Helper Function for Board
+    #frame = frame to draw board on 
+    #board = board to draw (pass the array itself ex: player1.my_board)
+    #size = size of button i.e. value of padx and pady
+    #offset_r = number of rows to offset by
+    #offset_c = number of columns to offset by
+def drawBoard(frame, board, size, offset_r, offset_c):
+    #Doesn't work right now. For now it will just creates a new board. 
+    for row_num in range(1,11): #iterate through rows
+        row_letter = int_to_char(row_num) # 1 = A, 2 = B, etc...
+        for col_num in range(1,11): #iterate through columns
+            button = Button(frame, text=(row_letter,col_num), padx=size, pady=size, fg='black').grid(row=row_num+offset_r, column=col_num+offset_c, sticky='nsew') 
+
+def board(type, size):
     if type == 'p1_set':
-        for row_num in range(1,11): #iterate through rows
-            row_letter = int_to_char(row_num) # 1 = A, 2 = B, etc...
-            for col_num in range(1,11): #iterate through columns
-                button = Button(frame4, text=(row_letter,col_num), padx=25, pady=25, fg='black').grid(row=row_num, column=col_num, sticky='nsew') 
+        #initialize player 1's board
+
+        #draw it
+        drawBoard(frame4, player1.my_board, size, offset_r=0, offset_c=0)
+        
     if type == 'p1_attack':
-        for row_num in range(1,11): #iterate through rows
-            row_letter = int_to_char(row_num) # 1 = A, 2 = B, etc...
-            for col_num in range(1,11): #iterate through columns
-                Button(frame9, text=(row_letter,col_num), padx=25, pady=25, fg='black').grid(row=row_num, column=col_num, sticky='nsew')
+        drawBoard(frame7, player1.my_board, size, offset_r=0, offset_c=0)
+        drawBoard(frame7, player1.enemy_board, size, offset_r=0, offset_c=12)
+        show_frame(frame7)
 
     if type == 'p2_set':
-        for row_num in range(1,11): #iterate through rows
-            row_letter = int_to_char(row_num) # 1 = A, 2 = B, etc...
-            for col_num in range(1,11): #iterate through columns
-                Button(frame5, text=(row_letter,col_num), padx=25, pady=25, fg='black').grid(row=row_num, column=col_num, sticky='nsew')
+        #initialize player 2's board
 
-    if type == 'p2_attack':
-        for row_num in range(1,11): #iterate through rows
-            row_letter = int_to_char(row_num) # 1 = A, 2 = B, etc...
-            for col_num in range(1,11): #iterate through columns
-                Button(frame7, text=(row_letter,col_num), padx=25, pady=25, fg='black').grid(row=row_num, column=col_num, sticky='nsew')
+        #draw it
+        drawBoard(frame5, player2.my_board, size, offset_r=0, offset_c=0)
+
+    if type == 'p2_attack': #Draw p2's board
+        drawBoard(frame9, player2.my_board, size, offset_r=0, offset_c=0)
+        drawBoard(frame9, player2.enemy_board, size, offset_r=0, offset_c=12)
+        show_frame(frame9)
 
 
 myButton1 = Button(frame2, text="1 ship ", padx=25, pady=25, command=partial(shipcount, 1), fg="black").grid(row=1, column=0)
@@ -135,7 +148,7 @@ def set_player_names(): #sets player names, then makes a label with the correspo
     p1_label = "Player 1 (" + player1.name + ")"
     frame4_label = Label(frame4, text=p1_label).grid(row=2, column=22)  
     
-    #set up frame 4 label
+    #set up frame 5 label
     p2_label = "Player 2 (" + player2.name + ")"
     frame4_label = Label(frame5, text=p2_label).grid(row=2, column=22) 
 
@@ -146,39 +159,38 @@ frame3_button = Button(frame3, text="Enter", command=partial(set_player_names)).
 #Frame 4 code   
     #label created inside set_player_names function
 frame4_button = Button(frame4, text="Finalize Ship\nPlacement", padx=20, pady=20, fg='black', command=partial(show_frame,frame5)).grid(row = 9, column = 22)
-board('p1_set')
+board('p1_set', 20)
 
 #frame 5 code
    #label created inside set_player_names function
-frame5_button = Button(frame5, text="Finalize Ship\nPlacement", padx=20, pady=20, fg='black', command=partial(show_frame,frame)).grid(row = 9, column = 22)
-board('p2_set')
+frame5_button = Button(frame5, text="Finalize Ship\nPlacement", padx=20, pady=20, fg='black', command=partial(show_frame,frame6)).grid(row = 9, column = 22)
+board('p2_set', 20)
 
 
 def checkWin(nextFrame):
-    win = 'false' #for now
-    if win == 'true':
+    win = False #for now
+    if win == True:
         show_frame(frame10) #show win frame
     else:
         show_frame(nextFrame)
 
 #frame 6 code = popup player 1
-frame6_button = Button(frame6, text="Ready Player 1?", padx=20, pady=20, fg='black', command=partial(board, "p2_attack")).grid()
+frame6_button = Button(frame6, text="Ready Player 1?", padx=20, pady=20, fg='black', command=partial(board, "p1_attack", 10)).place(anchor=CENTER, relx=0.5, rely=0.3,)
 
 
 #frame 7 = player 1 turn
 mylabel = Label(frame7, text="Select a grid to attack").grid(row=1, column=11)
-frame7_button = Button(frame7, text="Player 1 Done", padx=20, pady=20, fg='black', command=partial(checkWin, frame8)).grid(row=2, column=11)
+frame7_button = Button(frame7, text="Player 1 Done", padx=20, pady=20, fg='black', command=partial(checkWin, frame8)).grid(row=12, column=11)
 
 
 #frame 8 = popup player 2   
-frame8_button = Button(frame8, text="Ready Player 2?", padx=20, pady=20, fg='black', command=partial(checkWin, frame9)).grid()
+frame8_button = Button(frame8, text="Ready Player 2?", padx=20, pady=20, fg='black', command=partial(board, "p2_attack", 10)).place(anchor=CENTER, relx=0.5, rely=0.3,)
 
 #frame 9 = player 2 turn
-#frame 6 = player 1 turn
 mylabel = Label(frame9, text="Select a grid to attack").grid(row=1, column=11)   
-frame9_button = Button(frame9, text="Player 2 Done", padx=20, pady=20, fg='black', command=partial(checkWin, frame8)).grid(row=2, column=11)
+frame9_button = Button(frame9, text="Player 2 Done", padx=20, pady=20, fg='black', command=partial(checkWin, frame6)).grid(row=14, column=11)
 
 #Frame 10 = endscreen
-frame10_button = Button(frame10, text="Yay Player x Wins!!", padx=20, pady=20, fg='black', command=partial(checkWin, frame8)).grid()
+frame10_button = Button(frame10, text="Yay Player x Wins!!", padx=20, pady=20, fg='black').grid()
 
 root.mainloop()
