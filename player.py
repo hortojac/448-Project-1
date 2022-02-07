55ee8ec9ff459f9e6e6d3bcfb4a7899eb8f3da50
Julia Nichols <julia.nichols@ku.edu>
2022-02-07 11:28:11 -0600

    forgot to push player.py

--- player.py
+++ player.py
@@ -1,6 +1,16 @@
-from msilib.schema import Billboard
-
-
 class Player:
-    #myBoard
-    #enemyBoard
+    def __init__(self):
+        self.name = "NONE" #string for player's name
+        self.my_board = [] # board to show player's full board
+        self.enemy_board = [] #board to show player this player has marked on the other player's board
+                              # all buttons in my_board array will be DISABLED so you can't click your own grid
+
+         #A board will be a 100-element array that will stores the Tkinter IDs of each button
+        #Ex:
+        # [a1][a2][a3]....[b10]
+        # [b1][b2][b3]....[b10]
+        # .
+        # .
+        # .
+        # .
+        # [j1][j2][j3]....[j10]
