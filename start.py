5d28632f85eb963162ef6db645714f985536c2b3
Julia Nichols <julia.nichols@ku.edu>
2022-02-09 14:10:52 -0600

    placing second D deals with every bad user input

--- start.py
+++ start.py
@@ -457,44 +457,67 @@ def EnoughSpace_2(i, button_ids):#is the user orientating their ship in the corr
         if(i==current_index-1): #if i is to the left of index
             if(button_ids[i-1].cget('text') == "") and ((i-1)>=0) and ((i-1)%10!=9): #check that the next space to the left is empty
                 return(True)
-            if(button_ids[current_index+1].cget('text') == "") and ((current_index+1)<=99) and ((current_index+1)<=99): #check that the space to the right of the original is empty
+            elif(button_ids[current_index+1].cget('text') == "") and ((current_index+1)<=99) and ((current_index+1)<=99): #check that the space to the right of the original is empty
                 return(True)
         if(i==current_index+10): #if i is below the index
             if(button_ids[i+10].cget('text') == "") and ((i+10)<=99): #check that the next space down is empty
                 return(True)
-            if(button_ids[current_index-10].cget('text') == "") and ((current_index-10)>=0): #check that the space above the original is empty
+            elif(button_ids[current_index-10].cget('text') == "") and ((current_index-10)>=0): #check that the space above the original is empty
                 return(True)
         if(i==current_index-10): #if i is above the index
             if(button_ids[i-10].cget('text') == "") and ((i-10)>=0): #check that the next space up is empty
                 return(True)
-            if(button_ids[current_index+10].cget('text') == "") and ((current_index+10)<=99): #check that the space below the original is empty
+            elif(button_ids[current_index+10].cget('text') == "") and ((current_index+10)<=99): #check that the space below the original is empty
                 return(True)
         else:
             return(False)
     elif(placing_ships==7): #is the second D going to block the user from not being able to place the third and fourth D?    
-        #if i is to the right of index 
-            #check that the next 2 spaces to the right are empty
-            #check that the next space to the right is empty and 1 space to the left of the original index is empty
-            #check that 2 spaces to the left of the original index is empty
-        #if i is to the left of index 
-            #check that the next 2 spaces to the left are empty
-            #check that the next space to the left is empty and 1 space to the right of the original index is empty
-            #check that 2 spaces to the right of the original index is empty
-        #if i is below the index 
-            #check that the next 2 spaces down are empty
-            #check that the next space down is empty and 1 space above the original index is empty
-            #check that 2 spaces above the original index is empty
-        #if i is above the index
-            #check that the next 2 spaces up are empty
-            #check that the next space up is empty and 1 space below the original index is empty
-            #check that 2 spaces below the original index is empty
-        return(True)
+        if(i==current_index+1): #if i is to the right of index
+            if(button_ids[i+1].cget('text') == "") and ((i+1)<=99) and ((i+1)%10!=0) and (button_ids[i+2].cget('text') == "") and ((i+2)<=99) and ((i+2)%10!=0): #check that the next 2 spaces to the right are empty
+                return(True)
+            elif(button_ids[current_index-1].cget('text') == "") and ((current_index-1)>=0) and ((current_index-1)%10!=9) and (button_ids[current_index-2].cget('text') == "") and ((current_index-2)>=0) and ((current_index-2)%10!=9): #check that 2 spaces to the left of the original index are empty
+                return(True)
+            elif(button_ids[i+1].cget('text') == "") and ((i+1)<=99) and ((i+1)%10!=0) and (button_ids[current_index-1].cget('text') == "") and ((current_index-1)>=0) and ((current_index-1)%10!=9):#check that the next space to the right is empty and 1 space to the left of the original index is empty
+                return(True) 
+        if(i==current_index-1): #if i is to the left of index
+            if(button_ids[i-1].cget('text') == "") and ((i-1)>=0) and ((i-1)%10!=9) and (button_ids[i-2].cget('text') == "") and ((i-2)>=0) and ((i-2)%10!=9): #check that the next 2 spaces to the left are empty
+                return(True)
+            elif(button_ids[current_index+1].cget('text') == "") and ((current_index+1)<=99) and ((current_index+1)<=99) and (button_ids[current_index+2].cget('text') == "") and ((current_index+2)<=99) and ((current_index+2)<=99): #check that 2 spaces to the right of the original index is empty
+                return(True)
+            elif(button_ids[i-1].cget('text') == "") and ((i-1)>=0) and ((i-1)%10!=9) and (button_ids[current_index+1].cget('text') == "") and ((current_index+1)<=99) and ((current_index+1)<=99): #check that the next space to the left is empty and 1 space to the right of the original index is empty
+                return(True)
+        if(i==current_index+10): #if i is below the index
+            if(button_ids[i+10].cget('text') == "") and ((i+10)<=99) and (button_ids[i+20].cget('text') == "") and ((i+20)<=99): #check that the next 2 spaces down are empty
+                return(True)
+            elif(button_ids[current_index-10].cget('text') == "") and ((current_index-10)>=0) and (button_ids[current_index-20].cget('text') == "") and ((current_index-20)>=0): #check that 2 spaces above the original index is empty
+                return(True)
+            elif(button_ids[i+10].cget('text') == "") and ((i+10)<=99) and(button_ids[current_index-10].cget('text') == "") and ((current_index-10)>=0): #check that the next space down is empty and 1 space above the original index is empty
+                return(True)
+        if(i==current_index-10): #if i is above the index
+            if(button_ids[i-10].cget('text') == "") and ((i-10)>=0) and (button_ids[i-20].cget('text') == "") and ((i-20)>=0): #check that the next 2 spaces up are empty
+                return(True)
+            elif(button_ids[current_index+10].cget('text') == "") and ((current_index+10)<=99) and (button_ids[current_index+20].cget('text') == "") and ((current_index+20)<=99): #check that 2 spaces below the original index is empty
+                return(True)    
+            elif(button_ids[i-10].cget('text') == "") and ((i-10)>=0) and (button_ids[current_index+10].cget('text') == "") and ((current_index+10)<=99): #check that the next space up is empty and 1 space below the original index is empty
+                return(True)
+        else:
+            return(False)
     elif(placing_ships==11): #is the second E going to block the user from not being able to place the third, fourth, and fifth E?
+        if(i==current_index+1): #if i is to the right of index
+            if(button_ids[i+1].cget('text') == "") and ((i+1)<=99) and ((i+1)%10!=0) and (button_ids[i+2].cget('text') == "") and ((i+2)<=99) and ((i+2)%10!=0) and (button_ids[i+3].cget('text') == "") and ((i+3)<=99) and ((i+3)%10!=0): #check that the next 3 spaces to the right are empty
+                return(True)
+            elif(button_ids[current_index-1].cget('text') == "") and ((current_index-1)>=0) and ((current_index-1)%10!=9) and (button_ids[current_index-2].cget('text') == "") and ((current_index-2)>=0) and ((current_index-2)%10!=9): #check that 2 spaces to the left of the original index are empty
+                return(True)
+            elif(button_ids[i+1].cget('text') == "") and ((i+1)<=99) and ((i+1)%10!=0) and (button_ids[current_index-1].cget('text') == "") and ((current_index-1)>=0) and ((current_index-1)%10!=9):#check that the next space to the right is empty and 1 space to the left of the original index is empty
+                return(True) 
+        
         #if i is to the right of index 
             #check that the next 3 spaces to the right are empty
             #check that 3 spaces to the left of the original index are empty
             #check that the next 2 spaces to the right are empty and 1 space to the left of the original index is empty
             #check that the next space to the right is empty and 2 spaces to the left of the original index is empty
+        
+        
         #if i is to the left of index 
             #check that the next 3 spaces to the left are empty
             #check that 3 spaces to the right of the original index are empty
@@ -510,7 +533,6 @@ def EnoughSpace_2(i, button_ids):#is the user orientating their ship in the corr
             #check that 3 spaces below the original index are empty
             #check that the next 2 spaces up are empty and 1 space below the original index is empty
             #check that the next space up is empty and 2 spaces below the original index is empty
-        return(True)
     else:
         return(True)
 
